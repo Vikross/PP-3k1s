da3b4e7df25e2c6d7aa5a90d410d6c89a7d3191e
Vikross <viktorta.emelkina@gmail.com>
2024-01-05 02:25:11 +0500

    Обновление БД

--- database/models.py
+++ database/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Articles(models.Model):
-    location = models.CharField('Локация', max_length=50)
+    name = models.CharField('ФИО', max_length=50, blank=False)
+    location = models.CharField('Локация', max_length=50, blank=False)
     subdivision = models.CharField('Подразделение', max_length=50)
     department = models.CharField('Отдел', max_length=50)
-    group = models.CharField('Группа', max_length=50)
-    position = models.CharField('Должность', max_length=50)
-    name = models.CharField('ФИО', max_length=50)
-    kindofwork = models.CharField('Тип работы', max_length=50)
+    group = models.CharField('Группа', max_length=50, blank=False)
+    position = models.CharField('Должность', max_length=50, blank=False)
+    kindofwork = models.CharField('Тип работы', max_length=50, blank=False)
 
     def __str__(self):
         return self.location
